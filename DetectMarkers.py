48582a6c64ebd066ded53d24a8a8742ce8713acf
Evgeniy <evgeniy.gurilev@gmail.com>
2015-10-04 18:51:00 +0300

    Clustering algorithm was changed: the new one searches contours (OpenCV lib). Algorithm complexity is cubic, therefore has bad performance. Consider square marker with 4 points (quadratic complexity).

--- DetectMarkers.py
+++ DetectMarkers.py
@@ -3,6 +3,7 @@ from scipy import misc
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import cv2
 
 plt.ion()
 
@@ -19,7 +20,7 @@ def isTriple(p1, p2, p3, A):
     p1 = p1.astype(float)
     p2 = p2.astype(float)
     p3 = p3.astype(float)
-    Tol = 0.2#tolerance band
+    Tol = 0.15#tolerance band
 
     #are these points on the same line?
     #calulate a and b
@@ -57,55 +58,45 @@ def isTriple(p1, p2, p3, A):
 #############################################################
 
 #############################################################
-def isGrid(t1, t2, t3, AList):
+def isGrid(t1, t2, t3, AList, DistanceList):
     #test given three triples to be a grid. Triples must be
     #parallel to each other and equally spaced
     
     #are the triples parallel to each other?
-    Tol = 0.2#tolerance band
+    Tol = 0.15#tolerance band
     dA12 = abs(AList[0] - AList[1]) / ((abs(AList[0]) + abs(AList[1])) / 2)
     dA13 = abs(AList[0] - AList[2]) / ((abs(AList[0]) + abs(AList[2])) / 2)
     dA23 = abs(AList[1] - AList[2]) / ((abs(AList[1]) + abs(AList[2])) / 2)
-    if (dA12 + dA13 + dA23) / 3 < Tol:
+
+    #are the triples have equal length?
+    dD12 = abs(DistanceList[0] - DistanceList[1]) / ((abs(DistanceList[0]) + abs(DistanceList[1])) / 2)
+    dD13 = abs(DistanceList[0] - DistanceList[2]) / ((abs(DistanceList[0]) + abs(DistanceList[2])) / 2)
+    dD23 = abs(DistanceList[1] - DistanceList[2]) / ((abs(DistanceList[1]) + abs(DistanceList[2])) / 2)
+    if ((dA12 + dA13 + dA23) / 3 < Tol) and ((dD12 + dD13 + dD23) / 3 < Tol):
         return 1
     else:
         return 0
 #############################################################
 
 
-im = misc.imread('Image1.jpg')
+im = misc.imread('Image5.jpg')
 W = im.shape[0]
 L = im.shape[1]
-NumOfPoints = np.count_nonzero(im == 255)
-flag = np.zeros(NumOfPoints)
-R = 25
+im[im < 240] = 0
+#NumOfPoints = np.count_nonzero(im == 255)
 Clusters = []
-PointCoord = []
-seed = 0;
-seedSet = 1;
-clustering = 1
+#PointCoord = []
 
 
-for i in xrange(W):
-  for j in xrange(L):
-    if im[i][j] == 255:
-      PointCoord.append([i, j])
-while clustering:
-  Cluster = []
-  for n in range(NumOfPoints):
-    if not(flag[n]) and not(seedSet):
-      seed = n
-      seedSet = 1
-    if ((abs(PointCoord[seed][0] - PointCoord[n][0]) < R) and (abs(PointCoord[seed][1] - PointCoord[n][1]) < R) and seedSet):
-      Cluster.append(n)
-      flag[n] = 1
-  seedSet = 0
-  Clusters.append(Cluster)
-  if flag[NumOfPoints-1]:
-    clustering = 0
+_, contours0, hierarchy = cv2.findContours( im.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+Clusters = []
+for k in xrange(len(contours0)):
+    if len(contours0[k]) > 15 and len(contours0[k]) < 45:
+        Clusters.append(contours0[k])
 
 Clusters = np.array(Clusters)
-PointCoord = np.array(PointCoord)
+#PointCoord = np.array(PointCoord)
 
 plt.imshow(im)
 ClusterCoord = np.zeros([Clusters.shape[0], 2])
@@ -113,14 +104,15 @@ Y = np.zeros(Clusters.shape[0])
 
 #Find clusters' centres
 for i in xrange(Clusters.shape[0]):
-  ClusterCoord[i] = [PointCoord[Clusters[i]][:,0].mean().round(), PointCoord[Clusters[i]][:,1].mean().round()]
-  plt.text(ClusterCoord[i,1],ClusterCoord[i,0],i)
+  ClusterCoord[i] = [Clusters[i][:,0][:,0].mean().round(), Clusters[i][:,0][:,1].mean().round()]
+  plt.text(ClusterCoord[i,0],ClusterCoord[i,1],i)
 
 ClusterCoord = ClusterCoord.astype(int)
 plt.imshow(im)
 
 Triples = []
 Grids = []
+Distance = []
 A = []#list of line coefficients for triples (Ax + b)
 
 #Test 3 points to be the triple
@@ -134,9 +126,9 @@ for p1 in xrange(ClusterCoord.shape[0]):
             else:
                 if isTriple(ClusterCoord[p1,:],ClusterCoord[p2,:],ClusterCoord[p3,:], A):
                     Triples.append(sorted([p1, p2, p3]))
-                    print p1, p2, p3, A[-1]
+		    Distance.append(dist(ClusterCoord[p1,:], ClusterCoord[p3,:]))
+                    print p1, p2, p3, A[-1], Distance[-1]
 
-print
 
 #Test 3 triples to be parallel to each other:
 for t1 in xrange(len(Triples)):
@@ -151,7 +143,8 @@ for t1 in xrange(len(Triples)):
                 continue
             else:
                 AList = [A[t1], A[t2], A[t3]]
-                if isGrid(t1, t2, t3, AList):
+                DistanceList = [Distance[t1], Distance[t2], Distance[t3]]
+                if isGrid(t1, t2, t3, AList, DistanceList):
                     Grids.append(sorted([Triples[t1], Triples[t2], Triples[t3]]))
 		    #print Triples[t1]
                     #print Triples[t2]
@@ -165,7 +158,8 @@ for g1 in xrange(Grids.shape[0]):
         if np.all(np.transpose(Grids[g1]) == Grids[g2]):
             i = g1
             break
-print "Grid is found!"
+
 print Grids[i]
+print "Grid is found!"
 GridPts = np.reshape(Grids[i], 9)
-plt.plot(ClusterCoord[GridPts][:,1], ClusterCoord[GridPts][:,0],'o')
+plt.plot(ClusterCoord[GridPts][:,0], ClusterCoord[GridPts][:,1],'o')
