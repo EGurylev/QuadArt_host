19c495a2317bd26bd22d67fcc62ab2ad4a370ac8
Evgeniy <evgeniy.gurilev@gmail.com>
2015-12-05 19:01:52 +0300

    Top level module for configuration and sharing common variables across all project modules.

---
+++ root.py
@@ -0,0 +1,11 @@
+# Variables and constants shared across all modules
+
+F = 0.0
+tau_phi = 0.0
+tau_theta = 0.0
+tau_psi = 0.0
+k1 = 0.981; # coefficient which relates force with command signals
+k2 = 1; # coefficient which relates torque with command signals
+Tend = 6.0
+dt = 0.02
+t = 0
