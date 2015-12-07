042c5efc1c15e59ce6fb9537a50ff730328174f6
Evgeniy <evgeniy.gurilev@gmail.com>
2015-12-08 00:09:59 +0300

    Control loop calculation was moved to control.py module.

--- main.py
+++ main.py
@@ -3,18 +3,10 @@ import quad_model
 from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 import pyqtgraph.opengl as gl
-from control_lib import pid
+import control
 import math
 import numpy as np
 from scipy import integrate
-
-# model constants
-g = 9.81 # gravity acceleration
-M = 0.4 # quadrotor's mass
-l = 0.2 # length of arm
-Jxx = M * math.pow(l,2); # moment of inertia according to x axis
-Jyy = M * math.pow(l,2); # moment of inertia according to y axis
-Jzz = 2 * M * math.pow(l,2); # moment of inertia according to z axis
 
 # initial values
 PosGi = np.array([0,0,0]) # global position
@@ -29,26 +21,12 @@ view = gl.GLViewWidget()
 view.show()
 grid = gl.GLGridItem()
 view.addItem(grid)	
-PosInit = 0.5 * np.array([[-l,0,0,l,0],[0,l,-l,0,0],[0,0,0,0,0],[2,2,2,2,2]])
+PosInit = 0.5 * np.array([[-r.l,0,0,r.l,0],[0,r.l,-r.l,0,0],[0,0,0,0,0],[2,2,2,2,2]])
 pos = quad_model.AffineTransform(y[1], PosInit)
 scatter = pg.opengl.GLScatterPlotItem()
 scatter.setData(pos=pos[0:3,:].T,color=(1,0,0,.3))
 view.addItem(scatter)
 Timer = QtCore.QTimer()
-
-# Controller initialization
-ZController = pid(0.5,0,0.5,r.dt)
-ZController.setpoint = 0.0
-PsiController = pid(0.5,0,0.5,r.dt)
-PsiController.setpoint = 0
-
-XController = pid(0.2,0,0.1,r.dt)
-XController.setpoint = 1
-ThetaController = pid(0.1,0,0.1,r.dt)
-
-YController = pid(0.2,0,0.1,r.dt)
-YController.setpoint = 2.6
-PhiController = pid(0.1,0,0.1,r.dt)
 
 def update():
     global scatter, y, Timer
@@ -59,17 +37,7 @@ def update():
         scatter.setData(pos=pos[0:3,:].T,color=(1,0,0,.3))
         
         # Feedback control system
-        # Z control
-        r.F = ZController.evaluate(y[1][5])
-        r.F += M * g
-        # Cascade control of X
-        ThetaController.setpoint = XController.evaluate(y[1][3])# control X
-        r.tau_theta = ThetaController.evaluate(y[1][7])# control roll
-        # Cascade control of Y
-        PhiController.setpoint = -YController.evaluate(y[1][4])# control Y
-        r.tau_phi = PhiController.evaluate(y[1][6])# control pitch
-        # Yaw control
-        r.tau_psi = PsiController.evaluate(y[1][8])
+        r.F, r.tau_theta, r.tau_phi, r.tau_psi = control.control_loop(y)
     else:
         Timer.stop()
 
