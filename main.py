a01e7e10c3cd4a3a88761a016713f842711eda82
Evgeniy <evgeniy.gurilev@gmail.com>
2016-01-06 00:47:49 +0300

    MainUI top application class was added.

--- main.py
+++ main.py
@@ -10,86 +10,92 @@ import math
 import numpy as np
 import collections
 from scipy import integrate
+import sys
 
-def threadDone(frame, MarkerCenter, MeanLength):
-    global Dist, X, Y
-    imW.setImage(np.rot90(frame))
-    Dist = r.K / MeanLength # distance from camera to marker, m
-    X = r.L * MarkerCenter[0] / MeanLength # x coordinate of a marker, m
-    Y = - r.L * MarkerCenter[1] / MeanLength # y coordinate of a marker, m
-    plot_buffer_y.append(Dist)
-    plot_buffer_x.append(r.t)
-    PItem.setData(plot_buffer_x, plot_buffer_y)
+class MainUI(QtGui.QWidget):
 
-# Initial values
-PosGi = np.array([0,0,0]) # global position
-VelBi = np.array([0,0,0]) # body linear velocity
-AnglesGi = np.array([0,0,0]) # global angles (Euler)
-OmegaBi = np.array([0,0,0]) # body angular velocity
-y = np.concatenate([VelBi, PosGi, AnglesGi, OmegaBi])
-y = np.concatenate([[y],[y]])
-## Window initialization
+    def __init__(self, parent=None):
+        super(MainUI, self).__init__(parent)
+        self.y = np.concatenate([r.VelBi, r.PosGi, r.AnglesGi, r.OmegaBi])
+        self.y = np.concatenate([[self.y],[self.y]])
+
+        ## Set custom widgets
+        self.view3DW = gl.GLViewWidget()# the main 3D widget
+        self.plotW = pg.PlotWidget()# widget for 2D plots
+        self.imW = RawImageWidget()# image widget
+        # Set size policy for custom widgets
+        self.plotW.sizeHint = self.imW.sizeHint = lambda: pg.QtCore.QSize(400, 200)
+        self.view3DW.sizeHint = lambda: pg.QtCore.QSize(400, 400)
+        self.view3DW.setSizePolicy(self.plotW.sizePolicy())
+        self.imW.setSizePolicy(self.plotW.sizePolicy())
+        # Create a grid layout to manage the widgets size and position
+        self.layout = QtGui.QGridLayout()
+        self.setLayout(self.layout)
+        # Add widgets to the layout in their proper positions
+        self.layout.addWidget(self.view3DW, 0, 0, 2, 1)  # 3D widget goes on left side, spanning 2 rows
+        self.layout.addWidget(self.plotW, 0, 1)  # plot goes on upper-right
+        self.layout.addWidget(self.imW, 1, 1)  # plot goes on bottom-right
+        self.grid = gl.GLGridItem()# grid on 3D widget
+        self.view3DW.addItem(self.grid)
+        
+        self.pos = quad_model.AffineTransform(self.y[1], r.PosInit)
+        self.model_sc = pg.opengl.GLScatterPlotItem()
+        self.model_sc.setData(pos=self.pos[0:3,:].T,color=(1,0,0,.3))
+        self.exp_sc = pg.opengl.GLScatterPlotItem()
+        self.exp_sc.setData(pos=self.pos[0:3,:].T,color=(0,1,0,.3))
+        self.view3DW.addItem(self.model_sc)
+        self.view3DW.addItem(self.exp_sc)
+
+        # Search visual marker on quadrotor within its own thread
+        self.improc = DetectMarkers.ImageThread()
+        self.connect(self.improc, QtCore.SIGNAL("Sig(PyQt_PyObject, PyQt_PyObject, PyQt_PyObject)"), \
+            self.im_threadDone, QtCore.Qt.DirectConnection)
+        self.improc.start()
+        self.Dist = 0
+        self.X = 0
+        self.Y = 0
+
+        self.plot_buffer_y = collections.deque(np.zeros(100), maxlen=100)
+        self.plot_buffer_x = collections.deque(np.zeros(100), maxlen=100)
+        self.PItem = self.plotW.plot(self.plot_buffer_x, self.plot_buffer_y)
+        self.Timer = QtCore.QTimer()
+        self.Timer.timeout.connect(self.update)
+        self.Timer.start(1000 * r.dt)
+        
+
+    def im_threadDone(self, frame, MarkerCenter, MeanLength):
+        self.imW.setImage(np.rot90(frame))
+        self.Dist = r.K / MeanLength # distance from camera to marker, m
+        self.X = r.L * MarkerCenter[0] / 80.0 # x coordinate of a marker, m
+        self.Y = - r.L * MarkerCenter[1] / 80.0 # y coordinate of a marker, m
+        self.plot_buffer_y.append(self.X)
+        self.plot_buffer_x.append(r.t)
+        self.PItem.setData(self.plot_buffer_x, self.plot_buffer_y)
+    
+
+    def update(self):
+        ## Simulated
+        self.y = integrate.odeint(quad_model.RHS,self.y[1],np.array([r.t, r.t + r.dt]))
+        r.t = r.t + r.dt
+        pos = quad_model.AffineTransform(self.y[1], r.PosInit)
+        self.model_sc.setData(pos=pos[0:3,:].T,color=(1,0,0,.3))     
+        # Feedback control system
+        r.F, r.tau_theta, r.tau_phi, r.tau_psi = control.control_loop(self.y)
+        
+        ## Measured
+        y_m = [0, 0, 0, self.X, self.Dist, self.Y, 0, 0, 0]
+        pos = quad_model.AffineTransform(y_m, r.PosInit)
+        self.exp_sc.setData(pos=pos[0:3,:].T,color=(0,1,0,.3))
+        
+        
+    def closeEvent(self, event):
+        self.deleteLater()
+        self.Timer.stop()
+        self.improc.cap.release()
+        
+
+# Start up the main user-interface
 app = QtGui.QApplication([])
-
-## Define a top-level widget to hold everything
-mainW = QtGui.QWidget()
-
-## Set custom widgets
-view3DW = gl.GLViewWidget()# the main 3D widget
-plotW = pg.PlotWidget()# widget for 2D plots
-imW = RawImageWidget()# image widget
-# Set size policy for custom widgets
-plotW.sizeHint = imW.sizeHint = lambda: pg.QtCore.QSize(400, 200)
-view3DW.sizeHint = lambda: pg.QtCore.QSize(400, 400)
-view3DW.setSizePolicy(plotW.sizePolicy())
-imW.setSizePolicy(plotW.sizePolicy())
-# Create a grid layout to manage the widgets size and position
-layout = QtGui.QGridLayout()
-mainW.setLayout(layout)
-# Add widgets to the layout in their proper positions
-layout.addWidget(view3DW, 0, 0, 2, 1)  # 3D widget goes on left side, spanning 2 rows
-layout.addWidget(plotW, 0, 1)  # plot goes on upper-right
-layout.addWidget(imW, 1, 1)  # plot goes on bottom-right
-grid = gl.GLGridItem()# grid on 3D widget
-view3DW.addItem(grid)
-
-mainW.showMaximized()
-	
-PosInit = 0.5 * np.array([[-r.l,0,0,r.l,0],[0,r.l,-r.l,0,0],[0,0,0,0,0],[2,2,2,2,2]])
-pos = quad_model.AffineTransform(y[1], PosInit)
-model_sc = pg.opengl.GLScatterPlotItem()
-model_sc.setData(pos=pos[0:3,:].T,color=(1,0,0,.3))
-exp_sc = pg.opengl.GLScatterPlotItem()
-exp_sc.setData(pos=pos[0:3,:].T,color=(0,1,0,.3))
-view3DW.addItem(model_sc)
-view3DW.addItem(exp_sc)
-Timer = QtCore.QTimer()
-
-# Search visual marker on quadrotor within its own thread
-improc = DetectMarkers.ImageThread()
-mainW.connect(improc, QtCore.SIGNAL("Sig(PyQt_PyObject, PyQt_PyObject, PyQt_PyObject)"), threadDone, QtCore.Qt.DirectConnection)
-improc.start()
-
-plot_buffer_y = collections.deque(np.zeros(100), maxlen=100)
-plot_buffer_x = collections.deque(np.zeros(100), maxlen=100)
-PItem = plotW.plot(plot_buffer_x, plot_buffer_y)
-    
-
-def update():
-    global model_sc, exp_sc, y, Timer, Dist, X, Y
-    ## Simulated
-    y = integrate.odeint(quad_model.RHS,y[1],np.array([r.t, r.t + r.dt]))
-    r.t = r.t + r.dt
-    pos = quad_model.AffineTransform(y[1], PosInit)
-    model_sc.setData(pos=pos[0:3,:].T,color=(1,0,0,.3))     
-    # Feedback control system
-    r.F, r.tau_theta, r.tau_phi, r.tau_psi = control.control_loop(y)
-    
-    ## Measured
-    y_m = [0, 0, 0, X, Dist, Y, 0, 0, 0]
-    pos = quad_model.AffineTransform(y_m, PosInit)
-    exp_sc.setData(pos=pos[0:3,:].T,color=(0,1,0,.3))
-
-
-Timer.timeout.connect(update)
-Timer.start(1000 * r.dt)
+main_window = MainUI()
+main_window.showMaximized()
+sys.exit(app.exec_())
--- root.py
+++ root.py
@@ -1,9 +1,17 @@
 # Variables and constants shared across all modules
+import numpy as np
 
 # model constants
 g = 9.81 # gravity acceleration
 M = 0.4 # quadrotor's mass
 l = 0.2 # length of arm
+
+# Initial values
+PosGi = np.array([0,0,0]) # global position
+VelBi = np.array([0,0,0]) # body linear velocity
+AnglesGi = np.array([0,0,0]) # global angles (Euler)
+OmegaBi = np.array([0,0,0]) # body angular velocity
+PosInit = 0.5 * np.array([[-l,0,0,l,0],[0,l,-l,0,0],[0,0,0,0,0],[2,2,2,2,2]])
 
 # marker constants
 K = 88 # coefficient for Pinhole camera model
