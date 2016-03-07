385b95728c2daa4ef139276562d5b2191c5e3f9a
Evgeniy <evgeniy.gurilev@gmail.com>
2016-03-07 22:59:14 +0300

    Four points on marker detected using Harris corner detector. Function for marker testing was deleted.

--- QuadArt/DetectMarkers.py
+++ QuadArt/DetectMarkers.py
@@ -13,22 +13,24 @@ class ImageThread(QtCore.QThread):
         #Initial settings
         self.W, self.H = 1280, 720
         self.PerimeterPrev = 400
+        self.DebugFrame = []
         self.MarkerSize = self.PerimeterPrev / 4
         self.AreaPrev = self.MarkerSize * self.MarkerSize
-        self.MarkerCoord = np.array([self.W / 2, self.H / 2], dtype=np.int32)
+        self.MarkerCoord = np.array([self.W / 2, self.H / 2], dtype=np.int32)# Center of a marker
+        self.CornerCoord = np.zeros([4,2])
         self.WinScale = 1.8 # scale factor for search window
         
         self.cap = cv2.VideoCapture(0)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.W)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.H)
         self.MarkerFound = 0
-        self.Sig = QtCore.pyqtSignal(np.ndarray, list, float)
+        self.Sig = QtCore.pyqtSignal(np.ndarray, list, bool)
         
         
     def run(self):
         while True:
             self.update()
-            self.emit(QtCore.SIGNAL("Sig(PyQt_PyObject, PyQt_PyObject)"), self.RedframeC, self.MarkerSize)
+            self.emit(QtCore.SIGNAL("Sig(PyQt_PyObject, PyQt_PyObject)"), self.RedframeC, self.MarkerFound)
     
     
     def update(self):
@@ -38,7 +40,9 @@ class ImageThread(QtCore.QThread):
 
     def MeanShift(self, frame, MarkerCoord):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        FrameSize = gray.shape
         frame = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,27,-3)
+        self.DebugFrame = gray
         _, contours, _ = cv2.findContours( frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         Area = []
         Perimeter = []
@@ -78,27 +82,38 @@ class ImageThread(QtCore.QThread):
             Y = np.array(C[1::2])
             X = np.array(C[0::2])           
             MarkerCoord = np.array([int(X.mean()), int(Y.mean())])
-            
+            MarkerFound = self.FindCorners(X, Y, FrameSize)
         else:
             return (False, np.array([0, 0]))
                     
         return (MarkerFound, MarkerCoord)
-
-
-    def TestMarker(self):
-        Lengths = np.zeros(4)
-        for n in xrange(4):
-            if n == 3:
-                Lengths[n] = dist(self.MarkerCoord[3], self.MarkerCoord[0])
-            else:
-                Lengths[n] = dist(self.MarkerCoord[n], self.MarkerCoord[n + 1])
-
-        self.MeanLength = Lengths.mean()
         
-        if Lengths.std() / self.MeanLength < 0.1:
-            self.MarkerFound = 1
+      
+    def FindCorners(self, X, Y, FrameSize):
+        ContIm = np.zeros(FrameSize, np.float32)
+        ContIm[Y,X] = 1
+        dst = cv2.cornerHarris(ContIm,13,13,0.1)
+        _, dst = cv2.threshold(dst,0.5 * dst.max(),1,0)
+        dst = np.uint8(dst)
+        _, contoursCorn, _ = cv2.findContours( dst.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(self.frameC,str(len(contoursCorn)),(100,660), font, 0.8,(255,255,255),2,cv2.LINE_AA)
+        if len(contoursCorn) != 4:
+            MarkerFound = False
+            return MarkerFound
         else:
-            self.MarkerFound = 0
+            MarkerFound = True
+        i = 0
+        for cont in contoursCorn:
+            N = cont.shape[0]
+            C = cont.squeeze()
+            C = C.reshape(N * 2, 1)
+            Y = np.array(C[1::2])
+            X = np.array(C[0::2])           
+            self.CornerCoord[i][:] = np.array([int(X.mean()), int(Y.mean())])
+            i += 1
+        return MarkerFound    
+        
 
 
     def TrackMarker(self):
@@ -120,10 +135,14 @@ class ImageThread(QtCore.QThread):
         self.MarkerCoord -= np.array([X1, Y1])
         self.MarkerFound, self.MarkerCoord = self.MeanShift(MarkerFrame, self.MarkerCoord)
         self.MarkerCoord += np.array([X1, Y1])
+        self.CornerCoord += np.array([X1, Y1])
     
-        self.frameC = cv2.circle(self.frameC,tuple(self.MarkerCoord.astype(int)), 6, (255,0,), -1)
-        # Draw rectangle for marker reference
-        cv2.rectangle(self.frameC, (X1, Y1), (X2, Y2), (0,255,0), 1)
+        if self.MarkerFound:
+            self.frameC = cv2.circle(self.frameC,tuple(self.MarkerCoord.astype(int)), 6, (255,0,0), -1)
+            for corn in self.CornerCoord:
+                self.frameC = cv2.circle(self.frameC,tuple(corn.astype(int)), 5, (0,255,0), -1)
+            # Draw rectangle for marker reference
+            cv2.rectangle(self.frameC, (X1, Y1), (X2, Y2), (0,255,0), 1)
 
 
     def FindMarker(self):
@@ -132,14 +151,16 @@ class ImageThread(QtCore.QThread):
 
     def marker_search(self):
         ret, self.frameC = self.cap.read()
-        
+        e1 = cv2.getTickCount()
+            
         if self.MarkerFound:
              self.TrackMarker()
-             #MeanAreaHist.append(math.pow(self.MeanLength / 9.42, 2))
-             #RadiusHist.append(self.MarkerRadius)
         else:
              self.FindMarker()
-        
+        e2 = cv2.getTickCount()
+        time = 1000 * (e2 - e1)/ cv2.getTickFrequency()#ms
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(self.frameC,str(time),(100,450), font, 0.8,(255,255,255),2,cv2.LINE_AA)
         x1, y1 = self.W / 2, self.H / 2
         x2 = x1 + int(self.MarkerSize)        
         y2 = y1 + int(self.MarkerSize)
